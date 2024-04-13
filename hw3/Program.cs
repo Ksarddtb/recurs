b577a0368d9ddfeadeaed5e348ce58877f6bfcd1
ksard <flastdtb@yandex.ru>
2024-04-14 00:21:43 +0500

    homework3

---
+++ hw3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+void PrintArrayReverse(int[] arr, int index)
+{
+    if (index < 0) return;
+    Console.WriteLine(arr[index]);
+    PrintArrayReverse(arr, index - 1);
+}
+
+int[] array = { 1, 2, 3, 4, 5 };
+
+Console.WriteLine("Элементы массива, начиная с конца:");
+PrintArrayReverse(array, array.Length - 1);
