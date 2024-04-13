ac17ae0018e5c079774cde9672676ee61f602f8e
ksard <flastdtb@yandex.ru>
2024-04-14 00:07:28 +0500

    hw1

---
+++ hw1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+void PrintNaturalNumber(int min,int max){
+    if(min==max) return;
+    PrintNaturalNumber(min,max-1);
+    Console.WriteLine($"{max} ");
+}
+Console.WriteLine("Введите Начальное число");
+int min=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите конечное число");
+int max=Convert.ToInt32(Console.ReadLine());
+
+PrintNaturalNumber(min,max);
